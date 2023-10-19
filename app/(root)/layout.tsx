d37116541583603c4cdc8b01fdfab03b1e133e75
Marcus Senju <thegladus323@gmail.com>
2023-10-19 17:20:40 -0500

    arreglando error a la hora de iniciar sesion

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,44 +1,32 @@
-import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
+import { redirect } from 'next/navigation';
+import { auth } from '@clerk/nextjs';
 
-import prismadb from "@/lib/prismadb";
+import prismadb from '@/lib/prismadb';
 
 export default async function SetupLayout({
-    children,
-
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode
 }) {
-    /* `const { userId } = auth();` is destructuring the `userId` property from the return value of the
-    `auth()` function. The `auth()` function is likely a function provided by the `@clerk/nextjs`
-    library that returns information about the currently authenticated user. By destructuring
-    `userId`, the code is extracting the value of the `userId` property and assigning it to a
-    variable named `userId`. */
-    const { userId } = auth();
+  const { userId } = auth();
 
-    if (!userId) {
-        redirect('/sign-in');
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      userId,
     }
+  });
 
-    /* The code is using the `prismadb` library to query the database for a store record. It is calling
-    the `findFirst` method on the `store` object, passing in a `where` clause that specifies the
-    `userId` property should match the value of the `userId` variable. */
-    const store = await prismadb.store.findFirst({
-        where: {
-            userId
-        }
-    });
+  if (store) {
+    redirect(`/${store.id}`);
+  };
 
-    /* The `if (store)` condition checks if the `store` variable is truthy, meaning it has a value. If
-    the `store` variable has a value, it means that a store record was found in the database for the
-    current user. */
-    if (store) {
-        redirect(`/${store.id}`);
-    }
-
-    return(
-        <>
-            {children}
-        </>
-    );
+  return (
+    <>
+      {children}
+    </>
+  );
 };
--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 /* The code is exporting a default middleware function called `authMiddleware` from the `@clerk/nextjs`
 package. This middleware is used to protect routes and enforce authentication. */
 export default authMiddleware({
-    publicRoutes: ["/api/:path*"],
+    publicRoutes: ["/api/:path*", "/fe6c08d4-79bd-461c-a995-4395c9a31242"],
 });
 
 export const config = {
